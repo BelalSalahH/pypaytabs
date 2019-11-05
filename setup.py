c0fe82ef293cd9a209f90db3720bd2f110abae02
belal <belalsalah@slnee.com>
2019-11-05 13:01:20 +0300

    [ADD] Project Version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="pypaytabs",
-    version="1.0.0",
+    version="1.0.1",
     description="PyPaytabs is a Python library for Paytabs Payment Gateway.",
     long_description=README,
     long_description_content_type="text/markdown",
